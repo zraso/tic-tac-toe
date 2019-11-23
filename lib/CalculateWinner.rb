07df2f76b3e813fcb6ec299033f21b56095359e1
zraso <zaira.rasool@hotmail.co.uk>
2019-11-23 11:48:56 +0000

    Adds method to check player fields match game win grid

--- lib/CalculateWinner.rb
+++ lib/CalculateWinner.rb
@@ -1,10 +1,18 @@
 class CalculateWinner
 
   def check(player)
-    if player.fields.include? ('A1' && 'B2' && 'C3')
-      player
+    # puts player.fields
+    diagonal1 = ['A1', 'B2', 'C3']
+    diagonal2 = ['A3', 'B2', 'C1']
+    column1 = ['A1', 'B1', 'C1']
+    if player.fields == diagonal1
+      'Win'
+    elsif player.fields == diagonal2
+      'Win'
+    elsif player.fields == column1
+      'Win'
     else
-      nil
+      false
     end
   end
 
--- spec/win_checker_spec.rb
+++ spec/win_checker_spec.rb
@@ -1,20 +1,35 @@
 require 'calculatewinner'
 
 describe CalculateWinner do
-  let(:player_x) {double :player_x}
+  let(:player_x) {double :player_x, fields: []}
   let(:player_o) {double :player_o}
 
+  ## need to be testing whether it's a win or not. So check if the winning grids
+  ## matches what the player has
+
   describe '#check' do
-    it 'returns nil if play has no winner' do
+    it 'returns false if play has no winner' do
       allow(player_x).to receive(:fields) { %w[A1 B1 C2] }
       allow(player_o).to receive(:fields) { %w[A2 B2 B3] }
-      expect(subject.check(player_x)).to eq nil
+      expect(subject.check(player_x)).to eq false
     end
 
     it 'returns player if player has won with Diagonal 1' do
       allow(player_x).to receive(:fields) { %w[A1 B2 C3] }
       allow(player_o).to receive(:fields) { %w[A2 B3 C2] }
-      expect(subject.check(player_x)).to eq player_x
+      expect(subject.check(player_x)).to eq 'Win'
+    end
+
+    it 'returns player if player has won with Diagonal 2' do
+      allow(player_x).to receive(:fields) { %w[A3 B2 C1] }
+      allow(player_o).to receive(:fields) { %w[A1 B2 C2] }
+      expect(subject.check(player_x)).to eq 'Win'
+    end
+
+    it 'returns player if player has won with Column 1' do
+      allow(player_x).to receive(:fields) { ['A1', 'B1', 'C1'] }
+      allow(player_o).to receive(:fields) { %w[A2 B2 C3] }
+      expect(subject.check(player_x)).to eq "Win"
     end
 
   end
